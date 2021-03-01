b304a1f0d7ae4d32927f1db330c89e458e517ab1
nsbt <55523468+nsbt@users.noreply.github.com>
2021-03-02 01:02:20 +0900

    Create struct to handle arguments

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,25 @@ use std::io::prelude::*;
 fn main() {
     let args: Vec<String> = env::args()
         .collect();
-    let (query, filename) = parse_config(&args);
-    println!("Searching for {} in file {}", query, filename);
+    let config = parse_config(&args);
+    println!("Searching for {} in file {}", config.query, config.filename);
 
     let mut contents = String::new();
-    File::open(filename)
+    File::open(config.filename)
         .expect("file not found")
         .read_to_string(&mut contents)
         .expect("something went wrong reading the file");
     println!("with text:\n{}", contents);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    (&args[1], &args[2])
+struct Config {
+    query: String,
+    filename: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    Config {
+        query: args[1].clone(),
+        filename: args[2].clone(),
+    }
 }
